bb554138cf06b10ca3cfbb876e1243a7da93c042
varun2108 <varun.kamma16@gmail.com>
2020-09-01 20:41:19 +0530

    added customer service

---
+++ customer-serviece/src/main/java/com/example/customerserviece/CustomerServieceApplication.java
@@ -0,0 +1,13 @@
+package com.example.customerserviece;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerServieceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerServieceApplication.class, args);
+	}
+
+}
---
+++ customer-serviece/src/test/java/com/example/customerserviece/CustomerServieceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.customerserviece;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServieceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
